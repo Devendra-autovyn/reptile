7ff57b4b2635bb6025911c29c2b87eed6f1ede78
Dev Losal <devlosal70@gmail.com>
2025-08-27 06:39:04 +0000

    stills same issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,17 +13,16 @@ export default function Home() {
       <main className="flex-1 p-4 sm:p-6 lg:p-8">
         <div className="max-w-screen-2xl mx-auto grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
           
-          {/* Left Column */}
-          <div className="lg:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-8">
-            <div className="md:col-span-2">
-              <BalanceCard />
+          <div className="lg:col-span-2 flex flex-col gap-8">
+            <BalanceCard />
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+              <SecureVaultCard />
+              <SplitBillCard />
             </div>
-            <SecureVaultCard />
-            <SplitBillCard />
+            <InstantPayCard />
           </div>
 
-          {/* Right Column */}
-          <div className="lg:col-span-1 grid grid-cols-1 gap-8">
+          <div className="lg:col-span-1 flex flex-col gap-8">
             <GoalsCard />
             <RecentTransactions />
           </div>
