913ce551c2c70b3cc3a589cc917b5530b4bf7f0c
Dev Losal <devlosal70@gmail.com>
2025-08-27 06:43:00 +0000

    these are not in align should be in align to each other

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,14 +15,14 @@ export default function Home() {
           
           <div className="lg:col-span-2 flex flex-col gap-8">
             <BalanceCard />
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-              <SecureVaultCard />
-              <SplitBillCard />
-            </div>
             <InstantPayCard />
           </div>
 
           <div className="lg:col-span-1 flex flex-col gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-8">
+                <SecureVaultCard />
+                <SplitBillCard />
+            </div>
             <GoalsCard />
             <RecentTransactions />
           </div>
