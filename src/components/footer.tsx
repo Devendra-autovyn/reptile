272bb7d6b228b26acc79cd9801f921ec1e6e131c
Dev Losal <devlosal70@gmail.com>
2025-08-27 10:24:52 +0000

    increase the font size too in foter

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -13,9 +13,9 @@ export default function Footer() {
         className="flex items-center justify-center flex-col text-center h-full"
       >
         <div className="py-8">
-            <p className="text-lg font-bold tracking-wider text-foreground">Raptile</p>
-            <p className="text-sm text-muted-foreground">Fast. Secure. Limitless.</p>
-            <p className="text-xs text-muted-foreground/80 mt-4">
+            <p className="text-2xl font-bold tracking-wider text-foreground">Raptile</p>
+            <p className="text-base text-muted-foreground">Fast. Secure. Limitless.</p>
+            <p className="text-sm text-muted-foreground/80 mt-4">
             © {new Date().getFullYear()} Raptile. All rights reserved.
             </p>
         </div>
